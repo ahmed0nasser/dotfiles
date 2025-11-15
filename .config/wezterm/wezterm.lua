0de01c502ec11f102586bd4dbae08e61700d4f0c
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-11-15 20:42:50 +0200

    Fix zooming out glitches, change theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,23 +3,23 @@ local config = wezterm.config_builder()
 
 -- General
 config.font_size = 20
-config.line_height = 1.2
-config.font = wezterm.font_with_fallback({
-	"DejaVuSansM Nerd Font Mono",
-	"Noto Naskh Arabic",
-	"Noto Sans",
-})
+-- line_height is glitchy when zoom out
+-- config.line_height = 1.2
+config.font = wezterm.font("DejaVuSansM Nerd Font Mono")
+-- another good font for arabic
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.freetype_render_target = "HorizontalLcd"
 
 -- BIDI for Arabic support
 config.bidi_enabled = true
 config.use_ime = true
 
 -- Color Scheme
-config.color_scheme = 'Argonaut (Gogh)'
+config.color_scheme = 'Chester'
 
 -- Minimalization
 config.window_decorations = 'RESIZE'
-config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.warn_about_missing_glyphs = false
 
 -- Key bindings
