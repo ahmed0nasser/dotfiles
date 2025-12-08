529f55b0ac44b621d5291a6d9000086741cfb784
Ahmed Nasser <ahmed.n.abdulmajeed@gmail.com>
2025-12-08 22:15:28 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Enable Vi mode
+fish_vi_key_bindings
+# Cursor shapes for Vi modes
+set -g fish_cursor_insert    line     # Insert mode → line cursor (like Neovim)
+set -g fish_cursor_default   block    # Normal mode → block cursor
+set -g fish_cursor_visual    block    # Visual → block
+set -g fish_cursor_replace   underscore
+set -g fish_cursor_unknown   block
+
+# Aliasses
+alias sudo='sudo '
+alias vim='nvim '
+alias pn=pnpm
+# prayer-times Alexandria, Egypt
+alias salah='prayer-times --latitude 31.200092 --longitude 29.918739 --method Egyptian '
+
+# Make fzf use ripgrep
+if type rg &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='rg --files'
+  export FZF_DEFAULT_OPTS='-m'
+end
