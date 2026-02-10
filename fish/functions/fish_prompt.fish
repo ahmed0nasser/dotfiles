68f30526d2e585077d724daf6c05fe0a32b21804
Ahmed Nasser <ahmed.n.abdulmajeed@gmail.com>
2026-02-10 22:29:11 +0200

    Restructure dotfiles into packages

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,14 @@
+function fish_prompt
+        if test -n "$SSH_TTY"
+                echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+        end
+    
+        echo -n (set_color purple)(if test "$PWD" = "$HOME"; echo "~"; else; basename $PWD; end)' '    
+
+        set_color -o
+        if fish_is_root_user
+                echo -n (set_color yellow)'$ '
+        end
+        echo -n (set_color green)'❯ '
+        set_color normal
+end
